89631633edfbcccf5df877a2ef97e88865a9a910
jzzzzh <1499765600@qq.com>
2021-01-05 23:12:38 +0800

    main

---
+++ 简单/两数之和.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int a, b;
+        bool flag = false;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int temp = target - nums[i];
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[j] == temp)
+                {
+                    a = i;
+                    b = j;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                break;
+        }
+        return {a, b};
+    }
+};
